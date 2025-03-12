0b1f2420ac8b86e627e055530a34e8689c6d80f0
Kenley <knribnitez@gmail.com>
2025-03-12 16:48:25 +0800

    NAvGraph issue

--- app/src/main/java/com/example/hk_scholar_chat_app/navigation/RootNavGraph.kt
+++ app/src/main/java/com/example/hk_scholar_chat_app/navigation/RootNavGraph.kt
@@ -1,8 +1,20 @@
 package com.example.hk_scholar_chat_app.navigation
 
 import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import com.example.hk_scholar_chat_app.sealed_screens.AuthScreens
+import com.example.hk_scholar_chat_app.views.LoginScreen
 
 @Composable
-fun SetRootNavGraph(){
-
+fun SetRootNavGraph(navController: NavHostController){
+    NavHost(
+        startDestination = AuthScreens.Login.route,
+        navController = navController
+    ) {
+        composable(route = AuthScreens.Login.route) {
+            LoginScreen()
+        }
+    }
 }
