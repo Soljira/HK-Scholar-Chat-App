e53a9067bd59b15150bde7222456a4e3ba1c357e
Soljira <soljiragit@gmail.com>
2025-03-12 20:23:08 +0800

    none

--- app/src/main/java/com/example/hk_scholar_chat_app/HomeActivity.kt
+++ app/src/main/java/com/example/hk_scholar_chat_app/HomeActivity.kt
@@ -40,31 +40,36 @@ import kotlinx.coroutines.Dispatchers
 import java.util.UUID
 
 class HomeActivity : ComponentActivity() {
+
+    // Define the CoroutineScope at the class level
+    private val scope = CoroutineScope(Dispatchers.Main)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+//        enableEdgeToEdge()
         // 1 - Set up the OfflinePlugin for offline storage
         val offlinePluginFactory = StreamOfflinePluginFactory(appContext = applicationContext)
-        val statePluginFactory = StreamStatePluginFactory(config = StatePluginConfig(), appContext = this)
+        val statePluginFactory =
+            StreamStatePluginFactory(config = StatePluginConfig(), appContext = this)
 
         // 2 - Set up the client for API calls and with the plugin for offline storage
         // eto ung api key talaga natin. wala pang laman
-//        val client = ChatClient.Builder(getString(R.string.api_key), applicationContext)
-//            .withPlugins(offlinePluginFactory, statePluginFactory)
-//            .logLevel(ChatLogLevel.ALL) // Set to NOTHING in prod
-//            .build()
-
-        // eto ung sample api key from https://getstream.io/tutorials/android-chat/
-        // uncomment this and icomment ung nasa taas para matest nyo
-        val client = ChatClient.Builder("uun7ywwamhs9", applicationContext)
+        val client = ChatClient.Builder(getString(R.string.api_key), applicationContext)
             .withPlugins(offlinePluginFactory, statePluginFactory)
             .logLevel(ChatLogLevel.ALL) // Set to NOTHING in prod
             .build()
 
+        // eto ung sample api key from https://getstream.io/tutorials/android-chat/
+        // uncomment this and icomment ung nasa taas para matest nyo
+//        val client = ChatClient.Builder("uun7ywwamhs9", applicationContext)
+//            .withPlugins(offlinePluginFactory, statePluginFactory)
+//            .logLevel(ChatLogLevel.ALL) // Set to NOTHING in prod
+//            .build()
+
         // 3 - Authenticate and connect the user
         val user = User(
-            id = "tutorial-droid",
-            name = "Tutorial Droid",
+            id = "Ken22",
+            name = "Ken22",
             image = "https://bit.ly/2TIt8NR"
         )
         client.connectUser(
@@ -72,9 +77,7 @@ class HomeActivity : ComponentActivity() {
             // TODO: change this hardcoded token to something else later
             token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoidHV0b3JpYWwtZHJvaWQifQ.WwfBzU1GZr0brt_fXnqKdKhz3oj0rbDUm2DqJO_SS5U"
         ).enqueue() { result ->
-            /**
-             * CODE FROM https://getstream.io/chat/docs/android/?language=kotlin#channels
-             */
+
             if (result.isSuccess) {
                 // TODO: Handle success LATER NA TO
             } else {
@@ -82,49 +85,115 @@ class HomeActivity : ComponentActivity() {
                 println("Failed to connect user: ${result.getOrThrow() ?: "Unknown error"}")
             }
 
-        setContent {
-            val context = LocalContext.current
-            val clientInitialisationState by client.clientState.initializationState.collectAsState()
+            /**
+             * CODE FROM https://getstream.io/chat/docs/android/?language=kotlin#channels
+             */
+            val channelClient = client.channel(channelType = "messaging", channelId = "travel")
+            val extraData = mutableMapOf<String, Any>(
+                "name" to "Awesome channel about traveling"
+            )
 
-            ChatTheme {
-                Scaffold(
-                    modifier = Modifier.windowInsetsPadding(WindowInsets.statusBars)
-                ) { paddingValues ->
-                    Column(
-                        modifier = Modifier.padding(
-                            PaddingValues(
-                                top = paddingValues.calculateTopPadding(),
-                                start = 0.dp,
-                                end = 0.dp,
-                                bottom = 0.dp
-                            )
-                        )
-                    ) {
-                        when (clientInitialisationState) {
-                            InitializationState.COMPLETE -> {
-                                ChannelsScreen(
-                                    title = stringResource(id = R.string.app_name),
-                                    isShowingHeader = true,
-                                    onChannelClick = { channel ->
-                                        startActivity(ChannelActivity.getIntent(context, channel.cid))
-                                    },
-                                    onHeaderActionClick = {
-                                        println("Header Action Click")
+            // Creating a channel with the low level client
+            channelClient.create(memberIds = emptyList(), extraData = extraData).enqueue { result ->
+                if (result.isSuccess) {
+                    val channel: Channel = result.getOrThrow()
+                    println("channel creation success")
+                    // Use channel by calling methods on channelClient
+                } else {
+                    // Handle result.error()
+                }
+            }
 
-                                    },
-                                    onBackPressed = { finish() }
+
+            // Watching a channel's state using the offline library
+//            scope.launch {
+//                client.watchChannelAsState(cid = "messaging:travel", messageLimit = 0).collect { channelState ->
+//                    if (channelState != null) {
+//                        // StateFlow objects to observe
+//                        channelState.messages
+//                        channelState.reads
+//                        channelState.typing
+//                    } else {
+//                        // User not connected yet.
+//                    }
+//                }
+//            }
+            // Watching a channel's state using the offline library
+            val channelStateFlow = client.watchChannelAsState(cid = "messaging:travel", messageLimit = 0)
+
+            setContent {
+                val context = LocalContext.current
+                val clientInitialisationState by client.clientState.initializationState.collectAsState()
+
+                val channelState by channelStateFlow.collectAsState() // Collect the channel state here
+
+
+                ChatTheme {
+                    Scaffold(
+                        modifier = Modifier.windowInsetsPadding(WindowInsets.statusBars)
+                    ) { paddingValues ->
+                        Column(
+                            modifier = Modifier.padding(
+                                PaddingValues(
+                                    top = paddingValues.calculateTopPadding(),
+                                    start = 0.dp,
+                                    end = 0.dp,
+                                    bottom = 0.dp
                                 )
-                            }
-                            InitializationState.INITIALIZING -> {
-                                androidx.compose.material.Text(text = "Initialising...")
-                            }
-                            InitializationState.NOT_INITIALIZED -> {
-                                androidx.compose.material.Text(text = "Not initialized...")
+                            )
+                        ) {
+                            when (clientInitialisationState) {
+                                InitializationState.COMPLETE -> {
+                                    ChannelsScreen(
+                                        title = stringResource(id = R.string.app_name),
+                                        isShowingHeader = true,
+                                        onChannelClick = { channel ->
+                                            startActivity(
+                                                ChannelActivity.getIntent(
+                                                    context,
+                                                    channel.cid
+                                                )
+                                            )
+                                        },
+                                        onHeaderActionClick = {
+                                            println("Header Action Click")
+
+                                        },
+                                        onBackPressed = { finish() }
+                                    )
+
+                                    // Collect and display channel state data in the UI
+                                    if (channelState != null) {
+                                        val messages by channelState!!.messages.collectAsState()
+                                        val reads by channelState!!.reads.collectAsState()
+                                        val typing by channelState!!.typing.collectAsState()
+
+                                        Text(text = "Messages: ${messages.size}")
+                                        Text(text = "Reads: ${reads.size}")
+//                                        Text(text = "Typing: ${typing.size}")
+                                    } else {
+                                        Text(text = "Channel not connected yet.")
+                                    }
+                                }
+
+
+
+
+                                InitializationState.INITIALIZING -> {
+                                    androidx.compose.material.Text(text = "Initialising...")
+                                }
+
+                                InitializationState.NOT_INITIALIZED -> {
+                                    androidx.compose.material.Text(text = "Not initialized...")
+                                }
                             }
                         }
                     }
                 }
-            }
-        }
 
+            }
+
+
+        }
+    }
 }
